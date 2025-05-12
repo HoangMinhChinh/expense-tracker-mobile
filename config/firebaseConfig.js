12438965eca2efc7eba8aac3c6ea7c8f61dabb46
HoangMinhChinh <hoangminhchinh0109@gmail.com>
2025-05-12 15:37:39 +0700

    cập nhật api firebasefirebase

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -8,9 +8,9 @@ const firebaseConfig = {
   apiKey: "AIzaSyAofb4dbQAF0vsyzUAKRZT-AXItcCeN9wE",
   authDomain: "ltdnt-2056c.firebaseapp.com",
   projectId: "ltdnt-2056c",
-  storageBucket: "ltdnt-2056c.appspot.com", // ✅ đúng domain
-  messagingSenderId: "210473608593",
-  appId: "1:210473608593:web:c43088df1dbb9e81e1e8f0",
+  storageBucket: "ltdnt-2056c.firebasestorage.app",
+  messagingSenderId: "210473680593",
+  appId: "1:210473680593:web:c43088f1dbb9e8e11e0ff0",
   measurementId: "G-DS1CSVNWS4"
 };
 
